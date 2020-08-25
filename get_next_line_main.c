24aeb2c1780e4f935af3c130af60a5b87c503200
Luiz Barreta <lbarreta@student.42sp.org.br>
2020-08-25 20:18:24 -0300

    sending to test

--- get_next_line_main.c
+++ get_next_line_main.c
@@ -6,7 +6,7 @@
 /*   By: lbarreta <lbarreta@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 21:56:12 by lbarreta          #+#    #+#             */
-/*   Updated: 2020/08/25 20:17:51 by lbarreta         ###   ########.fr       */
+/*   Updated: 2020/08/25 13:53:09 by lbarreta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,57 @@ int		get_next_line(int fd, char **line)
 	if (ft_strlen(rest) > 1)
 		return (ft_split_line (rest, line));
 	return (0);
+}
+
+int main() {
+
+	int fd;
+	int a;
+	char *linha;
+
+	printf("Lendo arquivo ...\n");
+	//open arquivo
+	fd = open("arquivo",O_RDONLY);
+
+	a = get_next_line(fd, &linha);
+	printf("linha1: %s\n",linha);
+	printf("retorno da função: %i\n",a);
+	printf("================\n");
+
+	a = get_next_line(fd, &linha);
+	printf("linha2: %s\n",linha);
+	printf("retorno da função: %i\n",a);
+	printf("================\n");
+
+	a = get_next_line(fd, &linha);
+	printf("linha3: %s\n",linha);
+	printf("retorno da função: %i\n",a);
+	printf("================\n");
+
+	a = get_next_line(fd, &linha);
+	printf("linha4: %s\n",linha);
+	printf("retorno da função: %i\n",a);
+	printf("================\n");
+
+	a = get_next_line(fd, &linha);
+	printf("linha5: %s\n",linha);
+	printf("retorno da função: %i\n",a);
+	printf("================\n");
+
+	a = get_next_line(fd, &linha);
+	printf("linha6: %s\n",linha);
+	printf("retorno da função: %i\n",a);
+	printf("================\n");
+
+	a = get_next_line(fd, &linha);
+	printf("linha7: %s\n",linha);
+	printf("retorno da função: %i\n",a);
+	printf("================\n");
+
+	a = get_next_line(fd, &linha);
+	printf("linha8: %s\n",linha);
+	printf("retorno da função: %i\n",a);
+	printf("================\n");
+
+	close(fd);
 }
